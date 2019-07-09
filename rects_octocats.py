4cf6b26a5e847bf166717b35a91c12edb737261e
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-09 09:24:07 -0600

    Update rects_octocats.py

--- rects_octocats.py
+++ rects_octocats.py
@@ -75,8 +75,8 @@ def newImage():
 # ScreenWipe
 ###################################
 def ScreenWipe(direction):
-  global temp_image
-  global temp_draw
+  #global temp_image
+  #global temp_draw
   global bg_color
   temp_image = Image.new("RGB", (0, 0))
   temp_draw = ImageDraw.Draw(temp_image)
