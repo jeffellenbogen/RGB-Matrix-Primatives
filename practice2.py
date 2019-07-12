d7a44b79e100d7d9686572e32a9427e526e393de
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-12 12:30:16 -0600

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -13,8 +13,8 @@ matrix_rows = 32
 matrix_columns = 32 
 
 # how many matrixes stacked horizontally and vertically 
-matrix_horizontal = 4 
-matrix_vertical = 3
+matrix_horizontal = 2
+matrix_vertical = 1
 
 total_rows = matrix_rows * matrix_vertical
 total_columns = matrix_columns * matrix_horizontal
@@ -25,43 +25,44 @@ options.cols = matrix_columns
 options.chain_length = matrix_horizontal
 options.parallel = matrix_vertical 
 
-#options.hardware_mapping = 'adafruit-hat-pwm' 
+options.hardware_mapping = 'adafruit-hat-pwm' 
 #options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
-options.hardware_mapping = 'regular'  
+#options.hardware_mapping = 'regular'  
 
 options.gpio_slowdown = 2
 
 matrix = RGBMatrix(options = options)
-startRed = random.randint(100,150)
-startGreen = random.randint(0,200)
-startBlue = random.randint(10,255)
-fadeBlue =  random.randint(3,5)
+
+randomColor = random.randint(0,360)
+bg_color ="hsl({}, 100%, 20%)".format(randomColor)
 
 image = Image.new("RGB", (total_columns,total_rows))
 drawRect = ImageDraw.Draw(image)
 drawCircle = ImageDraw.Draw(image)
 
 for i in range (total_rows/2+1):
-  
-    drawRect.rectangle( (i,i,total_columns-i,total_rows-i), outline = (startRed,startGreen,startBlue-fadeBlue*i) )
-    sleep(.03)
-    matrix.SetImage(image, 0, 0)
+  randomColor = random.randint(0,360)
+  bg_color ="hsl({}, 100%, 20%)".format(randomColor)
+  drawRect.rectangle( (i,i,total_columns-i,total_rows-i), outline = bg_color ) 
+  sleep(.03)
+  matrix.SetImage(image, 0, 0)
   
 sleep(1)
 
 for i in range (total_rows/2+1):
-    #draw.rectangle( (i,i,total_columns-i, total_rows-i), outline = white)
-    drawCircle.ellipse(( i + (total_columns - total_rows), i,total_rows-i, total_rows-i), outline = (startRed,0,0) )
-    sleep(.03)
-    sleep(.03)
-    matrix.SetImage(image, 0, 0)
+  randomColor = random.randint(0,360)
+  bg_color ="hsl({}, 100%, 20%)".format(randomColor)
+  drawCircle.ellipse(( i + (total_columns - total_rows), i,total_rows-i, total_rows-i), outline = bg_color )
+  sleep(.03
+  matrix.SetImage(image, 0, 0)
 
 sleep(1)    
 
 for i in range (total_rows/2+1):
-  
-    drawRect.rectangle( (i,i,total_columns-i,total_rows-i), outline = (startRed,startGreen,startBlue-fadeBlue*i) )
-    sleep(.03)
-    matrix.SetImage(image, 0, 0)
+  randomColor = random.randint(0,360)
+  bg_color ="hsl({}, 100%, 20%)".format(randomColor)
+  drawRect.rectangle( (i,i,total_columns-i,total_rows-i), outline = bg_color )
+  sleep(.03)
+  matrix.SetImage(image, 0, 0)
   
 sleep(1)
