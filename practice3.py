c106471d9d9c19fa2e7c78e361e72f60b88e45f5
jeffellenbogen <jeffellenbogen@gmail.com>
2019-07-26 09:55:08 -0600

    practice 3 comitted

--- practice3.py
+++ practice3.py
@@ -15,8 +15,8 @@ matrix_rows = 32
 matrix_columns = 32 
 
 # how many matrixes stacked horizontally and vertically 
-matrix_horizontal = 2
-matrix_vertical = 1
+matrix_horizontal = 4
+matrix_vertical = 2
 
 total_rows = matrix_rows * matrix_vertical
 total_columns = matrix_columns * matrix_horizontal
@@ -27,9 +27,9 @@ options.cols = matrix_columns
 options.chain_length = matrix_horizontal
 options.parallel = matrix_vertical 
 
-options.hardware_mapping = 'adafruit-hat-pwm' 
+#options.hardware_mapping = 'adafruit-hat-pwm' 
 #options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
-#options.hardware_mapping = 'regular'  
+options.hardware_mapping = 'regular'  
 
 options.gpio_slowdown = 2
 
@@ -44,6 +44,7 @@ drawRect = ImageDraw.Draw(image)
 drawText = ImageDraw.Draw(image)
 
 colorInc = 0
+white = (255,255,255)
 
 while True:
   width = random.randint(2, total_columns//2)
@@ -58,9 +59,12 @@ while True:
   colorInc+=5
   if colorInc > 360:
     colorInc = 0
-  text_color = "hsl({}, 100%, 50%)".format(colorInc)  
+  
+  white = (255,255,255)
   black = (0,0,0)
-  drawText.text((2,5), "HELLO", font=fnt, fill=text_color)
+  #text_color = "hsl({}, 100%, 50%)".format(colorInc)  
+  text_color = white  
+  drawText.text((2,5), " NAR IS COOL ", font=fnt, fill=text_color)
   matrix.SetImage(image, 0, 0)    
   sleep(pause)
 
