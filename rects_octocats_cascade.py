460c4970e3e57838139c695e5831f093ca4bc3ae
jeffellenbogen <jeffellenbogen@gmail.com>
2019-07-29 21:50:35 +0100

    changes

--- rects_octocats_cascade.py
+++ rects_octocats_cascade.py
@@ -41,9 +41,9 @@ bg_color ="hsl({}, 100%, 20%)".format(randomColor)
 temp_image = Image.new("RGB", (total_columns,total_rows))
 temp_draw = ImageDraw.Draw(temp_image)
 
-imageSize = 28
+imageSize = 38
 slot=1
-imageSlots = 4
+imageSlots = 2
 
 ###################################
 # Background
