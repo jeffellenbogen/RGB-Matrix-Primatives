2e70cbd720b1aca9c66a6a83a6186488f8833184
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-30 13:38:58 -0600

    Update practice3.py

--- practice3.py
+++ practice3.py
@@ -1,4 +1,5 @@
 # Used in main loop
+# Used in main loop
 from time import sleep
 import random
 
@@ -6,17 +7,15 @@ import random
 # Graphics imports, constants and structures
 ###################################
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
-from PIL import Image, ImageDraw, ImageFont
-
-fnt = ImageFont.truetype('Pillow/Tests/fonts/DejaVuSans.ttf', 18)
+from PIL import Image, ImageDraw
 
 # this is the size of ONE of our matrixes. 
 matrix_rows = 32 
 matrix_columns = 32 
 
 # how many matrixes stacked horizontally and vertically 
-matrix_horizontal = 4
-matrix_vertical = 2
+matrix_horizontal = 4 
+matrix_vertical = 3
 
 total_rows = matrix_rows * matrix_vertical
 total_columns = matrix_columns * matrix_horizontal
