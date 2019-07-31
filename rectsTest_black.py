b6347d3533f69f3765964ba37752487184fd6a73
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-31 11:01:22 -0600

    Create rectsTest_black.py

---
+++ rectsTest_black.py
@@ -0,0 +1,63 @@
+
+# Used in main loop
+from time import sleep
+import random
+
+###################################
+# Graphics imports, constants and structures
+###################################
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image, ImageDraw
+
+# this is the size of ONE of our matrixes. 
+matrix_rows = 32 
+matrix_columns = 32 
+
+# how many matrixes stacked horizontally and vertically 
+matrix_horizontal = 4 
+matrix_vertical = 3
+
+total_rows = matrix_rows * matrix_vertical
+total_columns = matrix_columns * matrix_horizontal
+
+options = RGBMatrixOptions()
+options.rows = matrix_rows 
+options.cols = matrix_columns 
+options.chain_length = matrix_horizontal
+options.parallel = matrix_vertical 
+
+
+'''options.hardware_mapping = 'adafruit-hat-pwm' 
+'''
+#options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+options.hardware_mapping = 'regular'  
+
+options.gpio_slowdown = 2
+
+matrix = RGBMatrix(options = options)
+red = (50,0,0)
+blue = (0,0,255)
+green = (0,255,0)
+white = (255,255,255)
+black = (0,0,0)
+
+###################################
+# Main loop 
+###################################
+image = Image.new("RGB", (total_columns,total_rows))
+draw = ImageDraw.Draw(image)
+
+while True:
+
+  
+    draw.rectangle( (0,0,total_columns,total_rows), fill = black)
+    matrix.SetImage(image, 0, 0)
+  
+
+try:
+  print("Press CTRL-C to stop")
+  while True:
+    sleep(100)
+except KeyboardInterrupt:
+  exit(0)
+
